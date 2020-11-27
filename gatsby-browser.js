34339ee31a86585e976212b1862e1fbfdfce47d8
Lucas zapico <zapicolucas@gmail.com>
2020-11-27 11:32:23 -0800

    Change syntax to es6 import and Add hook into Gatsby browser first start * Syntax change for consistancy hook for troubleshooting and reference *

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,8 @@
-require('prismjs/themes/prism.css')
-require('prismjs/themes/prism-solarizedlight.css')
+import React from 'react'
+// import Scrollbar from 'react-smooth-scrollbar'
+import 'prismjs/themes/prism.css'
+import 'prismjs/themes/prism-solarizedlight.css'
+
+export const onClientEntry = () => {
+  window.addEventListener('load', () => {})
+}
