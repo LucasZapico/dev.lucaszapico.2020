ff9e36c87eab2d06c43fad0c53a855061526239c
Lucas zapico <zapicolucas@gmail.com>
2020-11-27 11:41:49 -0800

    Enhance formating via linter and prettier rules * *

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,21 +5,17 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-import LayoutLite from "./layout-lite"
-
-import Footer from "./footer"
-import Nav from "./nav"
-
+import React from 'react'
+import PropTypes from 'prop-types'
+import { useStaticQuery, graphql } from 'gatsby'
+import LayoutLite from './layout-lite'
+import Footer from './footer'
+import Nav from './nav'
 const Layout = ({ children }) => {
   return (
     <LayoutLite>
       <Nav />
-      <main className="main">
-      {children}
-      </main>
+      <main className="main">{children}</main>
       <Footer />
     </LayoutLite>
   )
